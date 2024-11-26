c2b66551cd81a259e2fe1814488a658b992f093c
Yoshida <rrr27x@gmail.com>
2024-11-26 22:52:06 +0900

    index.html,q&a,Js追加

--- js/script.js
+++ js/script.js
@@ -9,3 +9,19 @@ $(window).scroll(function () {
     $("footer p.pagetop").fadeOut(500);
   }
 });
+
+
+// スクロールすると、下からふわっと出現する設定
+$(window).scroll(function () {
+  var scrollAnimationElm = document.querySelectorAll("section");
+  var scrollAnimationFunc = function () {
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 100;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add("on");
+      }
+    }
+  };
+  window.addEventListener("load", scrollAnimationFunc);
+  window.addEventListener("scroll", scrollAnimationFunc);
+});
